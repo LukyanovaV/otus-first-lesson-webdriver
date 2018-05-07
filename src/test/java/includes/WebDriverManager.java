24b7447a8418131676dee84ebb523f553442c2a9
LukyanovaV <awakadosa170392>
2018-05-07 14:51:12 +0300

    Реализация паттерна Singletone для элемента WebDriver

--- src/test/java/includes/WebDriverManager.java
+++ src/test/java/includes/WebDriverManager.java
@@ -1,4 +1,38 @@
 package includes;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class WebDriverManager {
+    private static String browser;
+    private static WebDriver driver;
+
+    public static WebDriver getInstance(){
+        if(driver == null){
+        driver = WebDriverStart();
+    }
+    return driver;
+}
+
+    public String GetBrowser(String browser){
+        this.browser = browser;
+        return this.browser;
+    }
+
+    private static   WebDriver WebDriverStart() {
+        switch (browser) {
+            case "Chrome":
+                driver = new ChromeDriver();
+                break;
+            case "Firefox":
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                throw new IllegalStateException("Browser isn't correct!");
+        }
+        return driver;
+    }
+
 }
